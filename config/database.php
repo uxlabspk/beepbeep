3b913611af659e3b667bc4ea5f40a5680e8bc752
Muhammad Naveed <naveed@codehuntspk.com>
2026-03-27 15:10:07 +0500

    fix(config): update database password and hardcode contact info in footer

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 // Database configuration
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'root');
 define('DB_NAME', 'beepbeep_driving');
 
 // Create database connection
@@ -16,10 +16,3 @@ function getDbConnection() {
     $conn->set_charset("utf8mb4");
     return $conn;
 }
-
-// Site configuration
-define('SITE_NAME', 'Beep Beep Driving School');
-define('SITE_URL', 'http://localhost/beepbeep/');
-define('ADMIN_EMAIL', 'admin@beepbeepdriving.co.uk');
-define('PHONE_NUMBER', '01234 567 890');
-define('ADDRESS', '123 Driving Street, London, UK');
