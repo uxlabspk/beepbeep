ac24c5d17fbdaac71649af862ff9cc73ca3f1d7d
Muhammad Naveed <naveed@codehuntspk.com>
2026-04-12 16:50:56 +0500

    feat: adding areas we cover page

---
+++ areas.php
@@ -0,0 +1,13 @@
+<?php
+$pageTitle = 'Areas We Cover | Beep Beep Driving School';
+$currentPage = 'areas';
+include __DIR__ . '/includes/header.php';
+?>
+
+
+<main>
+    <h1>Areas We Cover</h1>
+</main>
+
+
+<?php include __DIR__ . '/includes/footer.php'; ?>
